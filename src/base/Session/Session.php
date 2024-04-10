11fdc4398c5e700e23ad4c75a3d35bfd7badfead
Shem <macapobresshem@gmail.com>
2024-04-11 01:03:04 +0800

    Added session Management

---
+++ src/base/Session/Session.php
@@ -0,0 +1,37 @@
+<?php
+namespace Base\Session;
+
+class Session {
+    const USER_KEY = 'user';
+
+    public static function start() {
+        if (session_status() == PHP_SESSION_NONE) {
+            session_start();
+        }
+    }
+
+    public static function set($key, $value) {
+        $_SESSION[$key] = $value;
+    }
+
+    public static function get($key) {
+        if (isset($_SESSION[$key])) {
+            return $_SESSION[$key];
+        }
+
+        return null;
+    }
+
+    public static function has($key) {
+        return isset($_SESSION[$key]);
+    }
+
+    public static function forget($key) {
+        unset($_SESSION[$key]);
+    }
+
+    public static function destroy() {
+        session_unset();
+        session_destroy();
+    }
+}
